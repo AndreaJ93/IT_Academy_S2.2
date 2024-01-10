99b1b6e75c8f70995ceb21ea13dacaeb07c60db2
Andrea Jordan <andreajt.1993@gmail.com>
2024-01-10 19:50:06 +0100

    Ecommerce S2.2

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,97 @@
 
+
 // Exercise 6
-function validate() {
+function validate(event) {
 	var error = 0;
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
 	var errorEmail = document.getElementById("errorEmail");  
+	var errorAddress = document.getElementById("errorAddress"); 
+	var errorLastN = document.getElementById("errorLastN"); 
+	var errorPassword = document.getElementById("errorPassword"); 
+	var errorPhone = document.getElementById("errorPhone"); 
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	if(fName.value === "" || fName.value.length < 3 || !/^[a-zA-Z]+$/.test(fName.value)){
+		fName.style.border = '2px solid red';
+    	errorName.style.display = 'block';
 		error++;
+	} else {
+		fName.style.border = '';
+    	errorName.style.display = 'none';
 	}
 
-	if(fEmail.value == ""){
+	if(fLastN.value === "" || fLastN.value.length < 3 || !/^[a-zA-Z]+$/.test(fLastN.value)){
+		fLastN.style.border = '2px solid red';
+    	errorLastN.style.display = 'block'
 		error++;
+	} else {
+		fLastN.style.border = '';
+    	errorLastN.style.display = 'none'
 	}
+
+	if(fEmail.value == "" || fEmail.value.length < 3 || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(fEmail.value)){
+		fEmail.style.border = '2px solid red';
+    	errorEmail.style.display = 'block'
+		error++;
+	} else {
+		fEmail.style.border = '';
+    	errorEmail.style.display = 'none'
+	}
+
+	if(fAddress.value === "" || fAddress.value.length < 3 || !/^[a-zA-Z0-9]+$/.test(fAddress.value)){
+		fAddress.style.border = '2px solid red';
+    	errorAddress.style.display = 'block'
+		error++;
+	} else {
+		fAddress.style.border = '';
+    	errorAddress.style.display = 'none'
+	}
+
+	if(fPhone.value == "" || fPhone.value.length < 3 || !/^[0-9]+$/.test(fPhone.value)){
+		fPhone.style.border = '2px solid red';
+    	errorPhone.style.display = 'block'
+		error++;
+	} else {
+		fPhone.style.border = '';
+    	errorPhone.style.display = 'none'
+	}
+
+	if(fPassword.value == "" || fPassword.value.length < 3 || !/^(?=.*[a-zA-Z])(?=.*[0-9])/.test(fPassword.value)){
+		fPassword.style.border = '2px solid red';
+    	errorPassword.style.display = 'block'
+		error++;
+	} else {
+		fPassword.style.border = '';
+    	errorPassword.style.display = 'none'
+	};
 	 
 	if(error>0){
 		alert("Error");
+		event.preventDefault();
 	}else{
 		alert("OK");
 	}
-
 }
+
+
+
+//print total and quantity:
+
+let totalPrice = localStorage.getItem("total");
+let totalQuantity = localStorage.getItem("quantity");
+
+
+document.getElementById("total_price").innerHTML = totalPrice;
+document.getElementById("count_product").innerHTML = totalQuantity;
+
+
+
--- js/shop.js
+++ js/shop.js
@@ -2,7 +2,7 @@
 var products = [
     {
         id: 1,
-        name: 'cooking oil',
+        name: 'Cooking oil',
         price: 10.5,
         type: 'grocery',
         offer: {
@@ -76,28 +76,130 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
+    // 1. Loop for to the array products to get the item to add to cart.
+    // 2. Add found product to the cart array.
+    for (let i = 0; i < products.length; i++) {
+        if (id === products[i].id) {
+            let productInCar = cart.find((prod) => prod.id === id);
+            if (productInCar === undefined) {
+                cart.push({...products[i], quantity: 1});
+            } else {
+                productInCar.quantity += 1;
+            };
+        }
+    }
+
+    //Change number to cart:
+    totalQuantity = 0;
+
+    for (let product of cart) {
+        totalQuantity += product.quantity 
+    }
+    document.getElementById("count_product").innerHTML = totalQuantity;
+    localStorage.setItem("quantity", totalQuantity);
+    return cart;
 }
 
 // Exercise 2
 function cleanCart() {
+    cart = [];
 
+    //Clean cart print:
+    let cartListShoppingCart = document.getElementById('cart_list');
+    cartListShoppingCart.innerHTML = '';
+
+    //Change total to 0:
+    document.getElementById("total_price").innerHTML = 0;
+
+    //Change number to cart:
+    document.getElementById("count_product").innerHTML = 0;
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    let total = 0;
+
+    for (let i = 0; i < cart.length; i++) {
+        let price = cart[i].price;
+        let quantity = cart[i].quantity;
+        total += price * quantity;
+    }
+    localStorage.setItem("total", total);
+    return total
 }
+
 
 // Exercise 4
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    let total = calculateTotal();
+    let subtotalWithDiscount = total;
+    let indexCookingOil = cart.findIndex((prod) => prod.id === 1);
+    let indexCupcakeMixture = cart.findIndex((prod) => prod.id === 3);
+    let cookingOilDiscount = 0;
+    let cupcakeMixtureDiscount = 0;
+    let cookingOilPrice = products[0].price;
+    let cupcakeMixturePrice = products[2].price;
+
+    
+    if (indexCookingOil !== -1 && cart[indexCookingOil].quantity >= 3) {
+        cookingOilDiscount = (cookingOilPrice * 0.20) * cart[indexCookingOil].quantity;
+        cart[indexCookingOil].price = (cookingOilPrice * (1 - 0.20)).toFixed(2);
+        subtotalWithDiscount -= cookingOilDiscount;
+    } else if (indexCookingOil !== -1 && cart[indexCookingOil].quantity < 3){
+        cart[indexCookingOil].price = cookingOilPrice;
+    };
+    
+    if (indexCupcakeMixture !== -1 && cart[indexCupcakeMixture].quantity >= 10) {
+        cupcakeMixtureDiscount = (cupcakeMixturePrice * 0.30) * cart[indexCupcakeMixture].quantity;
+        cart[indexCupcakeMixture].price = (cupcakeMixturePrice * (1 - 0.30)).toFixed(2);
+        subtotalWithDiscount -= cupcakeMixtureDiscount;
+    } else if (indexCupcakeMixture !== -1 && cart[indexCupcakeMixture].quantity < 10) {
+        cart[indexCupcakeMixture].price = cupcakeMixturePrice;
+    };
+
+    return subtotalWithDiscount;
 }
 
 // Exercise 5
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    applyPromotionsCart()
+    let total = calculateTotal();
+
+    let cartListShoppingCart = document.getElementById('cart_list');
+    cartListShoppingCart.innerHTML = '';
+
+    for (let i = 0; i < cart.length; i++) {
+        let row = document.createElement('tr');
+
+        let productName = document.createElement('th');
+        productName.textContent = cart[i].name;
+
+        let productPrice = document.createElement('td');
+        productPrice.textContent = cart[i].price; 
+
+        let productQuantity = document.createElement('td');
+        productQuantity.textContent = cart[i].quantity; 
+
+        let productTotal = document.createElement('td');
+        productTotal.textContent = (cart[i].price * cart[i].quantity).toFixed(2);
+
+        let removeButton = document.createElement('td');
+        removeButton.innerHTML = `<button onclick='removeFromCart(${cart[i].id})' class='border border-0 bg-transparent'>  ─ </button>`
+
+        row.appendChild(productName);
+        row.appendChild(productPrice);
+        row.appendChild(productQuantity);
+        row.appendChild(productTotal);
+        row.appendChild(removeButton);
+
+        cartList = cartListShoppingCart.appendChild(row);  
+    }
+
+    document.getElementById("total_price").innerHTML = total.toFixed(2);
+    
 }
 
 
@@ -105,8 +207,25 @@ function printCart() {
 
 // Exercise 7
 function removeFromCart(id) {
+    let product = cart.find((prod) => prod.id === id);
+    let prodIndex = cart.findIndex((cart) => cart.id === id);
+
+    if (product !== undefined && product.quantity === 1) {
+        cart.splice(prodIndex, 1);
+    } else if (product !== undefined && product.quantity > 1){
+        cart[prodIndex].quantity -= 1  
+    };
+
+    applyPromotionsCart();
+    printCart();
+
+    // Change number to cart: 
+    totalQuantity -= 1;
+    document.getElementById("count_product").innerHTML = totalQuantity;
+    localStorage.setItem("quantity", totalQuantity);
 
 }
+
 
 function open_modal() {
     printCart();
